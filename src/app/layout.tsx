47d091aa1a545bdae3ee02c4bd636e6fe248df48
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-11-15 23:01:06 -0300

    feat: change 3d portfolio to landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "./_components/ThemeProvider";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-sans",
+});
+
+const fontHeading = localFont({
+  src: "../shared/assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
+
+export const metadata: Metadata = {
+  title: "Rafael's Landing Page",
+  description: "Landing page for Rafael's personal website",
+  keywords: [
+    "Rafael",
+    "Rafael Madeira",
+    "Rafael's Landing Page",
+    "Rafael's",
+    "Landing Page",
+    "Personal Website",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={
+          (cn("min-h-screen bg-background font-sans antialiased"),
+          fontSans.variable,
+          fontHeading.variable)
+        }
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="">
+      <section id="intro">
+        <h1>
+          Hi, I am <span className="text-primary">Rafael</span>
+        </h1>
+      </section>
+    </main>
+  );
+}
