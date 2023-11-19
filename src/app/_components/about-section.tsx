c917bd5853361c62ff53f4c2e93e7acfb52279f9
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-11-19 20:51:15 -0300

    fix

--- src/app/_components/about-section.tsx
+++ src/app/_components/about-section.tsx
@@ -1,4 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
+interface SocialLinkProps {
+  Icon: any;
+  href: string;
+}
+function SocialLink({ Icon, ...props }: SocialLinkProps) {
+  return (
+    <Link className="">
+      <Icon clasName="h-6 w-6" />
+    </Link>
+  );
+}
 export default function AboutSection() {
   return (
     <section
@@ -7,7 +19,29 @@ export default function AboutSection() {
     >
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:gap-y-12 lg:grid-rows-[auto-1fr]">
         <div className="lg:pl-20 flex justify-center">
-          <div className="max-w-xs px-2.5 lg:max-w-none">{/* <Image /> */}</div>
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src="/avatar.jpg"
+              alt="Rafael Madeira"
+              width={800}
+              height={800}
+              quality="95"
+              priority={true}
+              className="aspect-square rotate-3 rounded-lg bg-zinc-100 dark:bg-zinc-800 object-cover"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2 text-center md:text-start">
+          <h1 className="font-heading text-3xl md:text-4xl lg:text-5xl">
+            @rafaelMadeira{" "}
+          </h1>
+          <p className="mt-6 text-muted-foreground">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
+            repudiandae blanditiis ipsum sint culpa totam nulla quam voluptate
+            assumenda, voluptatum nam provident non tempore maiores sapiente ex
+            error possimus maxime.
+          </p>
+          <div className="mt-6 flex justify-center md:justify-start gap-6"></div>
         </div>
       </div>
     </section>
